09e0c477738aff6e591f666402186f8855b2ba3b
Igor <igor.izhov@gmail.com>
2024-01-19 02:18:31 +0300

    +flake cfg

--- fpgenerator/celerytools/tools.py
+++ fpgenerator/celerytools/tools.py
@@ -60,19 +60,20 @@ def acoustID_lookup_celery_wrapper(self,*fp_args):
     print('fp:',fp_args)
     API_KEY = 'cSpUJKpD'
     meta = ["recordings","recordingids","releases","releaseids","releasegroups","releasegroupids", "tracks", "compress", "usermeta", "sources"]
-    response=acoustid.lookup(API_KEY, fp_args[1], fp_args[0],meta)
+    response = acoustid.lookup(API_KEY, fp_args[1], fp_args[0],meta)
     print(response)
 
-    return {'response':response,'fname':fp_args[2]}
+    return {'response': response,'fname': fp_args[2]}
 
-def MB_get_releases_by_discid_celery_wrapper(self,*discID_arg):	
+
+def MB_get_releases_by_discid_celery_wrapper(self, *discID_arg):
     discID = discID_arg[0]
     MB_discID_result = ''
     try:
-        MB_discID_result = musicbrainzngs.get_releases_by_discid(discID,includes=["artists","recordings","release-groups"])
+        MB_discID_result = musicbrainzngs.get_releases_by_discid(discID,includes=["artists", "recordings", "release-groups"])
     except Exception as e:
         print(e)
-        return {'RC':-6,'error':str(e)}
+        return {'RC': -6, 'error': str(e)}
 
     if 'disc' not in MB_discID_result:
         return {'RC':-7,'error':'DiskID MB - NOT detected','MB_discID_result':MB_discID_result}
@@ -83,12 +84,7 @@ def MB_get_releases_by_discid_celery_wrapper(self,*discID_arg):
 async def acoustID_lookup_wrapper(fp):
     API_KEY = 'cSpUJKpD'
     meta = ["recordings","recordingids","releases","releaseids","releasegroups","releasegroupids", "tracks", "compress", "usermeta", "sources"]
-    return acoustid.lookup(API_KEY, fp[1], fp[0],meta)	
-	
-	
-	
-async def acoustID_lookup_wrapper_parent(fp):
-    return await acoustID_lookup_wrapper(fp)	
+    return acoustid.lookup(API_KEY, fp[1], fp[0], meta)	
 
-			
-	
+async def acoustID_lookup_wrapper_parent(fp):
+    return await acoustID_lookup_wrapper(fp)
